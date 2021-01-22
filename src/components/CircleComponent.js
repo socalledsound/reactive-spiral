73d982e8fc036f3dd741bc853987a164886704bc
Chris Kubick <socalledsound@gmail.com>
2021-01-21 17:48:38 -0800

    end video 2

--- src/components/CircleComponent.js
+++ src/components/CircleComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CircleComponent = ({x,y, size, fillColor}) => {
     return ( 
-         <circle cx={x} cy={y} r={size} fill={fillColor} opacity="0.5"/> 
+         <circle cx={x} cy={y} r={size} fill={fillColor} opacity="0.4"/> 
      );
 }
  
